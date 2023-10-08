2d75226fb90977110ae13e2a57dcd51003846c3b
Kathlene Tajonera <kathlene.tajonera@gmail.com>
2023-10-08 11:32:43 +0800

    initial commit

---
+++ src/app/fonts.ts
@@ -0,0 +1,11 @@
+import { Antonio, League_Spartan } from 'next/font/google';
+
+export const antonio = Antonio({
+    subsets: ['latin'],
+    display: 'swap',
+});
+
+export const league_spartan = League_Spartan({
+    subsets: ['latin'],
+    display: 'swap',
+});
